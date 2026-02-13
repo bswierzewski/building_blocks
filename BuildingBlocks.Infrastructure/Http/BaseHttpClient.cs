149b8223b4145ca8a8c14eecb0906b62510b77b6
bswierzewski <swierzewski.bartosz@gmail.com>
2026-02-13 20:33:49 +0100

    Add ErrorOr

--- BuildingBlocks.Infrastructure/Http/BaseHttpClient.cs
+++ BuildingBlocks.Infrastructure/Http/BaseHttpClient.cs
@@ -1,53 +1,51 @@
 ﻿using System.Net.Http.Json;
+using ErrorOr;
 
 namespace BuildingBlocks.Infrastructure.Http;
 
 public abstract class BaseHttpClient(HttpClient httpClient)
 {
-        protected abstract Task ParseErrorAndThrowAsync(HttpResponseMessage response, CancellationToken ct);
+    protected abstract Task<List<Error>> ParseErrorAsync(HttpResponseMessage response, CancellationToken ct);
 
-        protected async Task<TResponse> GetAsync<TResponse>(string uri, CancellationToken ct)
+    protected async Task<ErrorOr<TResponse>> GetAsync<TResponse>(string uri, CancellationToken ct)
+    {
+        using var request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+        return await SendRequestAsync<TResponse>(request, ct);
+    }
+
+    protected async Task<ErrorOr<TResponse>> PostAsync<TRequest, TResponse>(string uri, TRequest payload, CancellationToken ct)
+    {
+        using var request = new HttpRequestMessage(HttpMethod.Post, uri)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            Content = JsonContent.Create(payload)
+        };
 
-            return await SendRequestAsync<TResponse>(request, ct);
-        }
+        return await SendRequestAsync<TResponse>(request, ct);
+    }
 
-        protected async Task<TResponse> PostAsync<TRequest, TResponse>(string uri, TRequest payload, CancellationToken ct)
+    protected async Task<ErrorOr<TResponse>> PutAsync<TRequest, TResponse>(string uri, TRequest payload, CancellationToken ct)
+    {
+        using var request = new HttpRequestMessage(HttpMethod.Put, uri)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Post, uri)
-            {
-                Content = JsonContent.Create(payload)
-            };
+            Content = JsonContent.Create(payload)
+        };
 
-            return await SendRequestAsync<TResponse>(request, ct);
-        }
+        return await SendRequestAsync<TResponse>(request, ct);
+    }
 
-        protected async Task<TResponse> PutAsync<TRequest, TResponse>(string uri, TRequest payload, CancellationToken ct)
-        {
-            using var request = new HttpRequestMessage(HttpMethod.Put, uri)
-            {
-                Content = JsonContent.Create(payload)
-            };
+    protected async Task<ErrorOr<TResponse>> SendRequestAsync<TResponse>(HttpRequestMessage request, CancellationToken ct)
+    {
+        using var response = await httpClient.SendAsync(request, ct);
 
-            return await SendRequestAsync<TResponse>(request, ct);
-        }
+        if (!response.IsSuccessStatusCode)
+            return await ParseErrorAsync(response, ct);
 
-        protected async Task<TResponse> SendRequestAsync<TResponse>(HttpRequestMessage request, CancellationToken ct)
-        {
-            using var response = await httpClient.SendAsync(request, ct);
+        var content = await response.Content.ReadFromJsonAsync<TResponse>(ct);
 
-            if (!response.IsSuccessStatusCode)
-            {
-                await ParseErrorAndThrowAsync(response, ct);
-                throw new InvalidOperationException("ParseErrorAndThrowAsync must throw an exception.");
-            }
+        if (content is null)
+            return Error.Unexpected("Http.EmptyResponse", "API returned an empty response body.");
 
-            var content = await response.Content.ReadFromJsonAsync<TResponse>(ct);
-
-            if (content is null)
-                throw new InvalidOperationException("API returned an empty response body.");
-
-            return content;
-        }
+        return content;
+    }
 }
