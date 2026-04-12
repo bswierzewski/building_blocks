30cf5c0913a78fc761cce9f33a8a76f206270e4e
bswierzewski <swierzewski.bartosz@gmail.com>
2026-04-12 07:13:55 +0200

    Add Roles to ICurrentUser

--- BuildingBlocks.Core/Abstractions/ICurrentUser.cs
+++ BuildingBlocks.Core/Abstractions/ICurrentUser.cs
@@ -9,4 +9,14 @@ public interface ICurrentUser
     /// Gets the identifier of the current user.
     /// </summary>
     Guid Id { get; }
+
+    /// <summary>
+    /// Gets the collection of roles assigned to the current user
+    /// </summary>
+    IEnumerable<string> Roles { get; }
+
+    /// <summary>
+    /// Checks if the current user has the specified role.
+    /// </summary>
+    bool IsInRole(string role);
 }
