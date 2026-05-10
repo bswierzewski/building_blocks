4185b41db98d6000590cd33f81f84d574901412d
bswierzewski <swierzewski.bartosz@gmail.com>
2026-05-10 20:30:57 +0200

    Remove ErrorOr

--- BuildingBlocks.Infrastructure/Http/BaseHttpClient.cs
+++ BuildingBlocks.Infrastructure/Http/BaseHttpClient.cs
@@ -1,18 +1,18 @@
 using System.Net.Http.Json;
-using ErrorOr;
+using BuildingBlocks.Core.Exceptions;
 
 namespace BuildingBlocks.Infrastructure.Http;
 
 public abstract class BaseHttpClient(HttpClient httpClient)
 {
-    protected async Task<ErrorOr<TResponse>> GetAsync<TResponse>(string uri, CancellationToken cancellationToken)
+    protected async Task<TResponse> GetAsync<TResponse>(string uri, CancellationToken cancellationToken)
     {
         using var request = new HttpRequestMessage(HttpMethod.Get, uri);
 
         return await SendRequestAsync<TResponse>(request, cancellationToken);
     }
 
-    protected async Task<ErrorOr<TResponse>> PostAsync<TRequest, TResponse>(string uri, TRequest payload, CancellationToken cancellationToken)
+    protected async Task<TResponse> PostAsync<TRequest, TResponse>(string uri, TRequest payload, CancellationToken cancellationToken)
     {
         using var request = new HttpRequestMessage(HttpMethod.Post, uri)
         {
@@ -22,7 +22,7 @@ public abstract class BaseHttpClient(HttpClient httpClient)
         return await SendRequestAsync<TResponse>(request, cancellationToken);
     }
 
-    protected async Task<ErrorOr<TResponse>> PutAsync<TRequest, TResponse>(string uri, TRequest payload, CancellationToken cancellationToken)
+    protected async Task<TResponse> PutAsync<TRequest, TResponse>(string uri, TRequest payload, CancellationToken cancellationToken)
     {
         using var request = new HttpRequestMessage(HttpMethod.Put, uri)
         {
@@ -32,24 +32,24 @@ public abstract class BaseHttpClient(HttpClient httpClient)
         return await SendRequestAsync<TResponse>(request, cancellationToken);
     }
 
-    protected async Task<ErrorOr<TResponse>> SendRequestAsync<TResponse>(HttpRequestMessage request, CancellationToken cancellationToken)
+    protected async Task<TResponse> SendRequestAsync<TResponse>(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         using var response = await httpClient.SendAsync(request, cancellationToken);
 
         if (!response.IsSuccessStatusCode)
         {
-            return await ParseErrorAsync(response, cancellationToken);
+            throw await ParseExceptionAsync(response, cancellationToken);
         }
 
         var content = await response.Content.ReadFromJsonAsync<TResponse>(cancellationToken);
 
         if (content is null)
         {
-            return Error.Unexpected("Http.EmptyResponse", "API returned an empty response body.");
+            throw new Exception("API returned an empty response body.");
         }
 
         return content;
     }
 
-    protected abstract Task<List<Error>> ParseErrorAsync(HttpResponseMessage response, CancellationToken cancellationToken);
+    protected abstract Task<Exception> ParseExceptionAsync(HttpResponseMessage response, CancellationToken cancellationToken);
 }
