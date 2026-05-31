fb3f14e531c259ea11d70de923996eab8f7ae2cb
bswierzewski <swierzewski.bartosz@gmail.com>
2026-05-31 07:39:24 +0200

    Add comments for env generator

--- BuildingBlocks.Infrastructure/Identity/Options/IdentityOptions.cs
+++ BuildingBlocks.Infrastructure/Identity/Options/IdentityOptions.cs
@@ -8,11 +8,15 @@ public sealed class IdentityOptions
 {
     public const string SectionName = "Authentication:Identity";
 
+    // @env: Authentication__Identity__Authority=
     public string? Authority { get; set; }
 
+    // @env: Authentication__Identity__Issuer=
     public string? Issuer { get; set; }
 
+    // @env: Authentication__Identity__Audience=
     public string? Audience { get; set; }
 
+    // @env: Authentication__Identity__SigningKey=
     public string? SigningKey { get; set; }
 }
